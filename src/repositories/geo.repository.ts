3b4c347f83e43640ba2533398b2f076b99d7abff
olnkravchenko <alenakravchenko1@gmail.com>
2025-04-15 15:13:05 +0300

    implement creation and selection

--- src/repositories/geo.repository.ts
+++ src/repositories/geo.repository.ts
@@ -1,49 +1,43 @@
 import { CountryDTO, CountryResponseDTO } from '../dtos/country.dto';
-import { GeoIPResponseDTO } from '../dtos/geoIp.dto';
+import { GeoIpDTO, GeoIPResponseDTO } from '../dtos/geoIp.dto';
 import { RegionDTO, RegionResponseDTO } from '../dtos/region.dto';
 
 export class GeoRepository {
-    constructor(private creds: Credential) {}
-
     /**
      * addIp
      */
-    public addIp(ip: string): GeoIPResponseDTO {
-        return { id: '123', address: '0.0.0.0' };
+    public async addIp(ip: GeoIpDTO): Promise<GeoIPResponseDTO> {
+        const rows = await prisma.geoIP.create({ data: ip });
+        return rows;
     }
 
     /**
      * getRegions
      */
-    public getRegions(): RegionResponseDTO[] {
-        return [
-            {
-                id: '123',
-                name: 'Donbass',
-                code: 'DNB',
-                country: { id: '123', name: 'Ukraine', isoCode: 'UA' },
-            },
-        ];
+    public async getRegions(): Promise<RegionResponseDTO[]> {
+        const rows = await prisma.region.findMany();
+        return rows;
     }
 
     /**
      * getCountries
      */
-    public getCountries(): CountryResponseDTO[] {
-        return [{ id: '123', name: 'Ukraine', isoCode: 'UA' }];
+    public async getCountries(): Promise<CountryResponseDTO[]> {
+        const rows = await prisma.country.findMany();
+        return rows;
     }
 
     /**
      * getIpByRegion
      */
-    public getIpByRegion(region: RegionDTO): GeoIPResponseDTO[] | null {
-        return [{ id: '123', address: '0.0.0.0' }];
-    }
+    // public getIpByRegion(region: RegionDTO): GeoIPResponseDTO[] | null {
+    //     return [{ id: '123', startIp: '0.0.0.0' }];
+    // }
 
     /**
      * getIpByCountry
      */
-    public getIpByCountry(country: CountryDTO): GeoIPResponseDTO[] | null {
-        return [{ id: '123', address: '0.0.0.0' }];
-    }
+    // public getIpByCountry(country: CountryDTO): GeoIPResponseDTO[] | null {
+    //     return [{ id: '123', address: '0.0.0.0' }];
+    // }
 }
