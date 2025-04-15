24abb476aef7a9f3df82e64c99e0e0bb19814c73
olnkravchenko <alenakravchenko1@gmail.com>
2025-04-15 15:12:05 +0300

    fix global client declaration

--- src/db/db-client.ts
+++ src/db/db-client.ts
@@ -2,7 +2,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: '../../build/.env' });
 
 const connectionString = process.env.DB_URL;
 
@@ -14,8 +14,14 @@ const adapter = new PrismaPg({ connectionString });
 
 // Declare global prisma type to prevent duplicate clients in dev
 declare global {
-    var prisma: PrismaClient | undefined;
+    var prisma: PrismaClient;
 }
-export const prisma =
-    global.prisma ||
+const client =
+    globalThis.prisma ??
     new PrismaClient({ adapter, log: ['query', 'warn', 'error'] });
+
+if (process.env.NODE_ENV !== 'prod') {
+    globalThis.prisma = client;
+}
+
+export const prisma = client;
