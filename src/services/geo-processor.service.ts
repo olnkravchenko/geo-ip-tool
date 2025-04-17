56372cc163e149ffcbd966f19f8acf8c1c93f624
olnkravchenko <alenakravchenko1@gmail.com>
2025-04-17 22:02:58 +0300

    add result monad

--- src/services/geo-processor.service.ts
+++ src/services/geo-processor.service.ts
@@ -1,22 +1,44 @@
+import { err, ok, Result } from 'neverthrow';
 import { CountryDTO } from '../dtos/country.dto';
-import { RegionDTO, RegionResponseDTO } from '../dtos/region.dto';
+import { GeoIPResponseDTO } from '../dtos/geoIp.dto';
+import { RegionDTO } from '../dtos/region.dto';
 import { GeoRepository } from '../repositories/geo.repository';
 
 export class GeoProcessorService {
-    constructor(private geo_rep: GeoRepository) {}
+    constructor(private geoRep: GeoRepository) {}
     /**
      * ip2location
      */
-    public ip2location(ips: string[]): RegionResponseDTO[] {
+    public ip2location(ips: string[]) {
         // parse ip into location
     }
 
     /**
      * getIpByRegion
      */
-    public getIpByRegion(region: RegionDTO) {
+    public async getIpByRegion({
+        name,
+        code,
+    }: RegionDTO): Promise<Result<GeoIPResponseDTO[], string>> {
         // check whether region exists
+        const regions = await this.geoRep.getRegions();
+
+        const filteredRegions = regions.filter(
+            (r) => r.name === (name ?? null) || r.code === (code ?? null),
+        );
+        if (filteredRegions.length == 0) {
+            return err('Region not found');
+        }
         // filter IPs by region
+        return ok([
+            {
+                id: '1',
+                startIp: 123n,
+                endIp: 456n,
+                countryId: '345',
+                regionId: null,
+            },
+        ]);
     }
 
     /**
