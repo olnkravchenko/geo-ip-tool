39e610782b2bc860d3d2cde65afe838bf0499f95
olnkravchenko <alenakravchenko1@gmail.com>
2025-04-29 15:36:40 +0300

    add server builder func

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,47 @@
 import FastifySensible from '@fastify/sensible';
 import dotenv from 'dotenv';
 import { fastify } from 'fastify';
+import { CountryRepository } from './repositories/country.repository';
+import { GeoRepository } from './repositories/geo.repository';
+import { RegionRepository } from './repositories/region.repository';
+import { GeoProcessorService } from './services/geo-processor.service';
 
 dotenv.config({ path: '../.env' });
 
 const PORT = Number(process.env.PORT) || 3000;
 const HOST = process.env.HOST || 'localhost';
 
-export const server = fastify();
-server.register(FastifySensible, { sharedSchemaId: 'HttpError' });
+const buildServer = async () => {
+    const server = fastify({ logger: true });
+    // add plugins
+    server.register(FastifySensible, { sharedSchemaId: 'HttpError' });
 
-server.get('/hello', async (req, reply) => {
-    return 'Hello world!\n';
-});
+    // initialize dependencies
+    const geoRepo = new GeoRepository();
+    const regionRepo = new RegionRepository();
+    const countryRepo = new CountryRepository();
+    const geoProcessorService = new GeoProcessorService(
+        geoRepo,
+        regionRepo,
+        countryRepo,
+    );
+    server.decorate('geoProcessorService', geoProcessorService);
 
-server.listen({ port: PORT, host: HOST }, (err, address) => {
-    if (err) {
-        console.error(err);
+    // basic routing
+    server.get('/hello', async (req, reply) => {
+        return 'Hello world!\n';
+    });
+
+    return server;
+};
+
+(async () => {
+    const app = await buildServer();
+    try {
+        const address = app.listen({ port: PORT, host: HOST });
+        app.log.info(`Server listening at ${address}`);
+    } catch (err) {
+        app.log.error(err);
         process.exit(1);
     }
-    console.log(`Server listening at ${address}`);
-});
+})();
