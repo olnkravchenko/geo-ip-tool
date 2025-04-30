b7ee0661aec4c979eb04709c7565f5cff4228e8e
olnkravchenko <alenakravchenko1@gmail.com>
2025-04-30 20:21:58 +0300

    change logging

--- src/routes/geo.ts
+++ src/routes/geo.ts
@@ -8,12 +8,12 @@ const geoRoutes: FastifyPluginAsync = async (app) => {
 
         result[0].match(
             (data: { ip: string; location: GeoIPRelatedResponseDTO }) => {
-                app.log.info(`200 OK ${ip}`);
+                req.log.info(`200 OK ${ip}`);
                 return reply.code(200).send(data);
             },
-            (cause: string) => {
-                app.log.error(cause);
-                return reply.code(404).send({ message: 'Not found' });
+            (cause) => {
+                req.log.error(cause);
+                return reply.code(404).send(cause.message);
             },
         );
     });
