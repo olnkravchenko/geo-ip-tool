a2ed549e7564ac5c699c3de430bd2064efbca6c0
olnkravchenko <alenakravchenko1@gmail.com>
2025-06-18 21:05:14 +0300

    add basic implementation

--- src/routes/location.ts
+++ src/routes/location.ts
@@ -1,9 +1,38 @@
 import { FastifyPluginAsync } from 'fastify';
 
 const locationRoutes: FastifyPluginAsync = async (app) => {
-    // TODO: add GET endpoint for converting coords to IP
-    // TODO: add GET endpoint to get all region IPs
-    // TODO: add GET endpoint to get all country IPs
+    app.get('/coords', async (req, reply) => {
+        const { lat, lon } = req.query as { lat: string; lon: string };
+        if (!lat || !lon) {
+            return reply.code(400).send({ error: 'Missing lat or lon' });
+        }
+        const result = await app.geoProcessorService.getIpByCoords(lat, lon);
+        return reply.code(200).send(result);
+    });
+
+    app.get('/region', async (req, reply) => {
+        const { name, isoCode } = req.query as {
+            name?: string;
+            isoCode?: string;
+        };
+        const result = await app.geoProcessorService.getIpByRegion({
+            name,
+            isoCode,
+        });
+        return reply.code(200).send(result);
+    });
+
+    app.get('/country', async (req, reply) => {
+        const { name, isoCode } = req.query as {
+            name?: string;
+            isoCode?: string;
+        };
+        const result = await app.geoProcessorService.getIpByCountry({
+            name,
+            isoCode,
+        });
+        return reply.code(200).send(result);
+    });
 };
 
 export default locationRoutes;
